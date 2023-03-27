f1becb388a08b5e89eca39baadb86c51cf650c89
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-03-27 01:14:49 -0300

    form page

--- moments/src/app/app.module.ts
+++ moments/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
+import { MomentFormComponent } from './components/moment-form/moment-form.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,8 @@ import { HomeComponent } from './components/pages/home/home.component';
     FooterComponent,
     AboutComponent,
     HomeComponent,
+    NewMomentComponent,
+    MomentFormComponent,
   ],
   imports: [
     BrowserModule,
