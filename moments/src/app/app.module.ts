9ddcf835fa0fcc27cf334e81038debe5ad8f092b
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-03-30 17:14:18 -0300

    Starting message component

--- moments/src/app/app.module.ts
+++ moments/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 import { MomentFormComponent } from './components/moment-form/moment-form.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MessagesComponent } from './components/messages/messages.component';
 
 @NgModule({
   declarations: [
@@ -22,13 +24,15 @@ import { MomentFormComponent } from './components/moment-form/moment-form.compon
     HomeComponent,
     NewMomentComponent,
     MomentFormComponent,
+    MessagesComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- moments/src/app/services/messages.service.ts
+++ moments/src/app/services/messages.service.ts
@@ -5,5 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class MessagesService {
 
+  message: string = '';
+
   constructor() { }
 }
