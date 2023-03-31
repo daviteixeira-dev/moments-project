fdc17719e0b49ef708a8bc0c7e9a2c4c2b8ee00b
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-03-31 16:48:22 -0300

    Single page at the moment

--- moments/src/app/app.module.ts
+++ moments/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NewMomentComponent } from './components/pages/new-moment/new-moment.com
 import { MomentFormComponent } from './components/moment-form/moment-form.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MessagesComponent } from './components/messages/messages.component';
+import { MomentComponent } from './components/pages/moment/moment.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { MessagesComponent } from './components/messages/messages.component';
     NewMomentComponent,
     MomentFormComponent,
     MessagesComponent,
+    MomentComponent,
   ],
   imports: [
     BrowserModule,
---
+++ moments/src/app/components/pages/moment/moment.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Moment } from 'src/app/Moment';
+import { Router, ActivatedRoute } from '@angular/router';
+import { MomentService } from 'src/app/services/moment.service';
+
+@Component({
+  selector: 'app-moment',
+  templateUrl: './moment.component.html',
+  styleUrls: ['./moment.component.css']
+})
+export class MomentComponent implements OnInit {
+
+  moment?: Moment;
+
+  constructor(private momentService: MomentService, private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+      const id = Number(this.route.snapshot.paramMap.get('id'));
+
+      this.momentService.getMoment(id).subscribe((item) => (this.moment = item.data));
+  }
+}
